b67953d039bbeee4ab32e556bca69e46f2cdcb27
Pavithra1405 <pavithramurugesan1405@gmail.com>
2026-06-10 01:58:20 +0530

    update CORS for production

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,10 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173"
+  origin: [
+    "http://localhost:5173",
+    "https://ai-voice-bot-nu.vercel.app/"  // your actual Vercel URL
+  ]
 }));
 app.use(express.json());
 
